b4bcfc7ea89173017fcbc8bfa8211e306fe9f910
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2024-07-02 21:21:18 +0530

    some auth fixes

--- src/components/Chat/chat.tsx
+++ src/components/Chat/chat.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { io, Socket } from "socket.io-client";
 import { auth } from "../../lib/firebase";
 import { Input } from "../ui/input";
@@ -34,6 +34,8 @@ const Chat: React.FC = () => {
     title: "",
     price: 0,
   });
+
+  const navigate = useNavigate();
 
   const socket: Socket = io(`${import.meta.env.VITE_API_URLL}`, {
     transports: ["websocket", "polling"],
@@ -112,42 +114,67 @@ const Chat: React.FC = () => {
 
   return (
     <div className="container mx-auto py-12">
-      <h1 className="text-5xl mb-2 font-bold tracking-tighter">
-        Chat for {item.title}
-      </h1>
-      <p className="text-xl mb-8 font-bold tracking-tighter">
-        Price:<span className="font-normal tracking-loose"> {item.price}</span>
-      </p>
-      <div className="flex flex-col space-y-4">
-        {messages.length > 0 &&
-          messages.map((msg, index) => (
-            <div
-              key={index}
-              className={`message p-4 rounded-3xl ${
-                msg.senderId === userSession?.uid
-                  ? "bg-blue-500 text-white self-end"
-                  : "bg-gray-200 text-gray-700 self-start"
-              }`}
+      {userSession ? (
+        <>
+          <h1 className="text-5xl mb-2 font-bold tracking-tighter">
+            Chat for {item.title}
+          </h1>
+          <p className="text-xl mb-8 font-bold tracking-tighter">
+            Price:
+            <span className="font-normal tracking-loose"> {item.price}</span>
+          </p>
+          <div className="flex flex-col space-y-4">
+            {messages.length > 0 &&
+              messages.map((msg, index) => (
+                <div
+                  key={index}
+                  className={`message p-4 rounded-3xl ${
+                    msg.senderId === userSession?.uid
+                      ? "bg-blue-500 text-white self-end"
+                      : "bg-gray-200 text-gray-700 self-start"
+                  }`}
+                >
+                  {msg.message}
+                </div>
+              ))}
+          </div>
+          <div className="flex flex-row items-center mt-4">
+            <Input
+              type="text"
+              value={newMessage}
+              onChange={(e) => setNewMessage(e.target.value)}
+              placeholder="Type your message..."
+              className="w-full px-4 py-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500"
+            />
+            <Button
+              className="ml-4 px-6 h-10 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
+              onClick={handleSendMessage}
             >
-              {msg.message}
-            </div>
-          ))}
-      </div>
-      <div className="flex flex-row items-center mt-4">
-        <Input
-          type="text"
-          value={newMessage}
-          onChange={(e) => setNewMessage(e.target.value)}
-          placeholder="Type your message..."
-          className="w-full px-4 py-2 rounded-lg border-gray-300 focus:outline-none focus:border-blue-500"
-        />
-        <Button
-          className="ml-4 px-6 h-10 py-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 focus:outline-none"
-          onClick={handleSendMessage}
-        >
-          Send
-        </Button>
-      </div>
+              Send
+            </Button>
+          </div>
+        </>
+      ) : (
+        <div className="border border-red-400 bg-red-200 rounded-2xl p-10 mx-auto">
+          <p className="text-center text-xl text-red-800 font-bold">
+            Please login to chat with the seller
+          </p>
+          <div className="flex flex-col w-1/2 mx-auto">
+            <Button
+              onClick={() => navigate("/login")}
+              className="mt-4 mx-auto flex"
+            >
+              Login to your account
+            </Button>{" "}
+            <Button
+              onClick={() => navigate("/signup")}
+              className="mt-4 mx-auto flex"
+            >
+              Create an account
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
