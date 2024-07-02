a5363ab8f83d4c4e05943f70e66069379b6646ef
AntonyJudeShaman <antonyjudeshaman@gmail.com>
2024-07-02 14:39:29 +0530

    refactor product page

--- src/components/Product/similarproducts.tsx
+++ src/components/Product/similarproducts.tsx
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import MyToast from "../ui/my-toast";
 import { cn } from "../../lib/utils";
 
 interface Item {
